0d15ddf2544aa7262f197e27ed97db3f1c2292f6
Mleon7 <2710887576@qq.com>
2023-08-23 15:32:17 +0800

    Task 2: ReLU

--- src/relu.s
+++ src/relu.s
@@ -12,27 +12,48 @@
 #   - If the length of the array is less than 1,
 #     this function terminates the program with error code 36
 # ==============================================================================
-relu:
-    # Prologue
 
+# i -> s0, array[i] -> t1
+# for (int i = 0; i != n; i++) {
+#   if (array[i] < 0) {
+#       array[i] = 0; 
+#   }
+# }
+
+relu:
+    addi t0, x0, 1
+    blt a1, t0, quit
+
+    # Prologue
+    addi sp, sp, -4
+    sw s0, 0(sp)
+    
+    addi s0, x0, 0
 
 loop_start:
+    
+    beq s0, a1, loop_end
 
-
-
-
-
-
+    lw t1, 0(a0)
+    bge t1, x0, loop_continue
+    sw x0, 0(a0)
 
 
 loop_continue:
 
-
+    addi s0, s0, 1
+    addi a0, a0, 4
+    j loop_start
 
 loop_end:
 
-
     # Epilogue
+    lw s0, 0(sp)
+    addi sp, sp, 4
 
 
     jr ra
+
+quit:
+    li a0 36
+    j exit
